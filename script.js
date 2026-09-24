86b4aaf499a435b769894dce90f15376d3115b05
aria-s <ariashirzadi@yahoo.com>
2026-09-24 15:07:08 -0400

    First javascript code

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Write your JavaScript below this line
+var firstName = "Aria Shirzadi";
+var favoriteSnack = "beef jerky";
+
+console.log(
+    "Hi, my name is " +
+    firstName +
+    " and I wish I had " +
+    favoriteSnack +
+    " right now."
+);
